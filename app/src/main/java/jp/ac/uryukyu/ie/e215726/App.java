f1637df729c67501279679d79fc3f2b7fac6c23a
crab85193 <crab85193@gmail.com>
2022-01-11 10:07:32 +0900

    [Remove] Remove AppTest.

--- app/src/main/java/jp/ac/uryukyu/ie/e215726/App.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e215726/App.java
@@ -4,11 +4,7 @@
 package jp.ac.uryukyu.ie.e215726;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
-
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+
     }
 }
